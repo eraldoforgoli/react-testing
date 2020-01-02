e3fb764d4cf72f7120daac46571044ebcda6ce10
Eraldo Forgoli <eraldoforgoli@gmail.com>
2020-01-02 21:23:08 +0100

    fix bug

--- examples/Button/Button.test.js
+++ examples/Button/Button.test.js
@@ -1,39 +1,29 @@
 // Button.test.js
 
-import Button from './Button.react';
-import expect from 'expect';
-import { shallow } from 'enzyme';
-import React from 'react';
+import Button from "./Button.react";
+import expect from "expect";
+import { shallow } from "enzyme";
+import React from "react";
 
-describe('<Button />', () => {
-  it('renders a <button>', () => {
-    const renderedComponent = shallow(
-      <Button></Button>
-    );
+describe("<Button />", () => {
+  it("renders a <button>", () => {
+    const renderedComponent = shallow(<Button></Button>);
 
-    expect(
-      renderedComponent.find("button").node
-    ).toExist();
+    expect(renderedComponent.find("button").node).toExist();
   });
 
-  it('renders text', () => {
+  it("renders text", () => {
     const text = "Click me!";
-    const renderedComponent = shallow(
-      <Button>{ text }</Button>
-    );
+    const renderedComponent = shallow(<Button>{text}</Button>);
 
-    expect(
-      renderedComponent.contains(text)
-    ).toEqual(true);
+    expect(renderedComponent.contains(text)).toEqual(true);
   });
 
-  it('handles clicks', () => {
+  it("handles clicks", () => {
     const onClickSpy = expect.createSpy();
-    const renderedComponent = shallow(
-      <Button onClick={onClickSpy} />
-    );
+    const renderedComponent = shallow(<Button onClick={onClickSpy} />);
 
-    renderedComponent.find('button').simulate('click');
+    renderedComponent.find("button").simulate("click");
 
     expect(onClickSpy).toHaveBeenCalled();
   });
