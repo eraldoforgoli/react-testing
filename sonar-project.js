b6f1176d542aff4ef390e4213ef0ff4f075c1997
Eraldo Forgoli <eraldoforgoli@gmail.com>
2020-01-02 22:24:12 +0100

    fix typo

--- sonar-project.js
+++ sonar-project.js
@@ -4,7 +4,7 @@ sonarqubeScanner(
     serverUrl: "http://173.249.41.180:9000/",
     options: {
       "sonar.sources": ".",
-      "sonar.inclusions": "projects" // Entry point of your code
+      "sonar.inclusions": "examples/**" // Entry point of your code
     }
   },
   () => {}
